922d15f187114a527ef7019d462797fab1a10874
ClementNjeru <njeruclement36@gmail.com>
2022-10-09 23:57:40 +0300

    updates the power_controller.rb file

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,14 @@
 class PowersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+    def index
+        render json: Power.all
+    end
+    def show
+        power = Power.find(params[:id])
+        render json: power
+    end
+    private
+    def not_found_response
+        render json: {error: 'Power Not Found' }
+    end
 end
