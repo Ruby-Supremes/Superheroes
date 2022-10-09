3d65d17413e43bf7e33b640448237f3450a9235d
ClementNjeru <njeruclement36@gmail.com>
2022-10-09 23:56:05 +0300

    updates the hero_controller.rb file

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,5 +1,14 @@
 class HerosController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
     def index
         render json: Hero.all
+    end
+    def show
+        hero = Hero.find(params[:id])
+        render json: hero, include: :powers
+    end
+    private
+    def not_found_response
+        render json: {error: 'Hero Not Found' }
     end
 end
