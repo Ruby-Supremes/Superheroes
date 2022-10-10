b54b96445064aa8b96c930fcead5fe18968bd11c
ClementNjeru <njeruclement36@gmail.com>
2022-10-10 23:28:09 +0300

    updates the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :powers, only: [:update, :index, :show]
-  resources :hero_powers
+  resources :hero_powers, only: [:create]
   resources :heros, only: [:index, :show]
 
 end
