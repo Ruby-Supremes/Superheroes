ea9be838225a79de539cda3717ff93e8abc466c2
ClementNjeru <njeruclement36@gmail.com>
2022-10-10 23:25:15 +0300

    updates the hero_powers controller

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,18 @@
 class HeroPowersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    def create
+        heroPower = HeroPower.create!(hero_power_params)
+        render json: heroPower
+    end
+
+    private
+    def hero_power_params
+        params.permit(:strength, :power_id, :hero_id)
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+      end
+
+
 end
