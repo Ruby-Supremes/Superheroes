bec40a08d7d9aeab7f34d51f2f934330b4344123
ClementNjeru <njeruclement36@gmail.com>
2022-10-10 23:25:59 +0300

    updates the powers controller

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,7 @@
 class PowersController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
     def index
         render json: Power.all
     end
@@ -13,14 +15,18 @@ class PowersController < ApplicationController
                 power.update(power_params)
                 render json: power
             else
-                render json: {error: "Power not found"}, status: :no_content
+                render json: { error: "Power not found" }, status: :no_content
             end
     end
     private
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+      end
     def not_found_response
-        render json: {error: 'Power Not Found' }, status: :no_content
+        render json: { error: 'Power Not Found' }, status: :no_content
     end
     def power_params
         params.permit(:name, :description)
       end
+      
 end
