548a59a7a8a6d9a2b0195994765ad40d09457ce3
ClementNjeru <njeruclement36@gmail.com>
2022-10-10 10:08:43 +0300

    updates the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  resources :powers
+  resources :powers, only: [:update, :index, :show]
   resources :hero_powers
   resources :heros, only: [:index, :show]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
